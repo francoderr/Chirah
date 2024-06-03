3377602d0f48e7e6fe697204237e1bd71a915229
Frank Onjoro <frank.onjoro@swerri.io>
2024-06-03 16:54:27 +0300

    added normal user features

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
 import { login, signUp } from '../controllers/userController.js'
+import { createReport, getReports } from '../controllers/reportController.js'
 
 const router  = express.Router()
 
 router.post('/signup', signUp)
 router.post('/login', login)
+router.post('/createReport', createReport)
+router.get('/getReports', getReports)
 
 export default router
